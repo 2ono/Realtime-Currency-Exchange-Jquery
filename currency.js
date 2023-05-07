c950158d24afd527319b64df8e0ac2a206b200a9
2ono <clt3555@gmail.com>
2023-05-07 23:41:27 +0900

    not working yet

--- currency.js
+++ currency.js
@@ -1,7 +1,7 @@
-let appkey = "";
-$(function () {
-  appkey = "ded6bce1cc3ad107e22ba9dd";
-  // bind currency code dropdown
+let appkey = "ded6bce1cc3ad107e22ba9dd";
+
+console.log("Hello");
+$("#btnsubmit").on("click", function () {
   $.get(
     "https://v6.exchangerate-api.com/v6/" + appkey + "/codes",
     {},
@@ -13,8 +13,8 @@ $(function () {
       });
       $("#ddlfrom").append(row);
       $("#ddlTo").append(row);
-      $("#ddlfrom").val("USD");
-      $("#ddlTo").val("EUR");
+      $("#ddlfrom").val("KRW");
+      $("#ddlTo").val("USD");
       calculate();
     }
   ).fail(function (d) {
@@ -22,7 +22,32 @@ $(function () {
     console.log(d.reponseText);
   });
 });
-// calculatre currency conversion
+
+// let appkey = "";
+// $(function () {
+//   appkey = "ded6bce1cc3ad107e22ba9dd";
+//   // bind currency code dropdown
+//   $.get(
+//     "https://v6.exchangerate-api.com/v6/" + appkey + "/codes",
+//     {},
+//     function (data) {
+//       let row = "";
+//       $.each(data.supported_codes, function (i, v) {
+//         row +=
+//           "<option value='" + v[0] + "'>" + v[0] + "-" + v[1] + "</option>";
+//       });
+//       $("#ddlfrom").append(row);
+//       $("#ddlTo").append(row);
+//       $("#ddlfrom").val("USD");
+//       $("#ddlTo").val("EUR");
+//       calculate();
+//     }
+//   ).fail(function (d) {
+//     alert("Error while calling the API");
+//     console.log(d.reponseText);
+//   });
+// });
+// // calculatre currency conversion
 function calculate() {
     $.get("https://v6.exchangerate-api.com/v6/"+ appkey +"/pair/"+$("#ddlfrom").val()+"/"+$("#ddlTo").val()+"/"+Number($("#txtamount").val())+"", function(data) {
         $("#txtoutput").val(data.conversion_result);
@@ -31,3 +56,6 @@ function calculate() {
         console.log(d.reponseText);
       });
 }
+
+
+
